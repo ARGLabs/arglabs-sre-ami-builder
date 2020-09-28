ae3a4a20572e3c374fd7bbb34611eecd6edacad7
Andre Ramoni <andre.ramoni@gmail.com>
2020-09-28 12:27:08 -0300

    .

---
+++ packer/provision.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+##############################
+# This script is called by packer and
+# already run with sudo.
+
+echo
+echo "-------------------------------------------"
+echo "    Provisioning script."
+echo "-------------------------------------------"
+#uname -a
+#yum update -y
+
+if [ "`cat /etc/system-release 2>> /dev/null | grep Amazon`" != "" ]; then
+  amazon-linux-extras install epel -y
+  amazon-linux-extras install ansible2 -y
+else
+  yum install -y epel-release
+  yum install -y ansible
+fi
+
+
---
+++ packer/run.sh
@@ -0,0 +1,2 @@
+packer build -var-file=vars-amazon2.json build-ami.json
+
