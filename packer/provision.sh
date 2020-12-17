02d05b4cee9184b41ee412687361b09a44371f5f
Andre Ramoni <andre.ramoni@gmail.com>
2020-12-17 17:20:03 -0300

    .

--- packer/provision.sh
+++ packer/provision.sh
@@ -10,12 +10,7 @@ echo "-------------------------------------------"
 uname -a
 yum update -y
 
-if [ "`cat /etc/system-release 2>> /dev/null | grep Amazon`" != "" ]; then
-  amazon-linux-extras install epel -y
-  amazon-linux-extras install ansible2 -y
-else
-  yum install -y epel-release
-  yum install -y ansible
-fi
+amazon-linux-extras install epel -y
+amazon-linux-extras install ansible2 -y
 
 
