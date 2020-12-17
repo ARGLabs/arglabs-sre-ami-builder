ae723ef5f1944f3d73ca9a4bb15b082386b36919
Andre Ramoni <andre.ramoni@gmail.com>
2020-12-17 17:00:09 -0300

    .

--- packer/run.sh
+++ packer/run.sh
@@ -1,2 +1,3 @@
-packer build -var-file=vars-amazon2.json build-ami.json
+packer build -var-file=vars-amazon2-arm64.json build-ami.json
+packer build -var-file=vars-amazon2-x86_64.json build-ami.json
 
